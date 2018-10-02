fe751454b1ffbe530c5fe03779c8e70365f8a90e
sysadmin29 <sysadmin29@cccdms.com>
2018-10-02 14:57:53 +0800

    added layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,91 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit-no">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/gif/png" href="images/mini%20icon.png">
+    <title>CCCDMSI - Payroll</title>
+</head>
+<body>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+<section>
+    <nav class="navbar navbar-dark fixed-top flex-md-nowrap shadow">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#"><img src="images/ccc.png" alt="ccc" height="55px" width="70px">CCC Data Management Services Inc.</a>
+        <ul class="navbar-nav px-3">
+            <li class="nav-item text-nowrap">
+                <a class="nav-link" href="#">Sign out</a>
+            </li>
+        </ul>
+    </nav>
+</section>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+<section>
+    <div class="inside-body">
+        <div class="row">
+            <div class="sidebar col-md-2">
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+                <input class="form-control ml-4" type="text" placeholder="Search" aria-label="Search">
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+                <div class="btn-group-vertical">
+                    <button type="button" class="btn btn-primary" data-toggle="collapse">Dashboard</button>
+                    <button type="button" class="btn btn-primary" data-toggle="collapse">Leave</button>
+                    <button type="button" class="btn btn-primary" data-toggle="collapse">Overtime</button>
+                    <button type="button" class="btn btn-primary" data-toggle="collapse">Reports</button>
+                    <button type="button" class="btn btn-primary dropdown" data-toggle="collapse" data-target="#demo" >Managed Payroll <ion-icon name="arrow-dropdown" class="float-right"></ion-icon></button>
+                    <div id="demo" class="collapse">
+                        <button type="button" class="btn btn-secondary">Payroll</button>
+                        <div class="dropdown-divider"></div>
+                        <button type="button" class="btn btn-secondary">Payroll Period</button>
+                        <button type="button" class="btn btn-secondary">Shift Schedule</button>
+                    </div>
+                    <button type="button" class="btn btn-primary" data-toggle="collapse">Holidays</button>
+                    <button type="button" class="btn btn-primary" data-toggle="collapse">Employee</button>
+                    <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo1" >Setup</button>
+                    <div id="demo1" class="collapse">
+                        <button type="button" class="btn btn-secondary">SSS Table</button>
+                        <button type="button" class="btn btn-secondary">BIR</button>
+                        <button type="button" class="btn btn-secondary">Phil Health</button>
+                        <button type="button" class="btn btn-secondary">Pagibig</button>
+                    </div>
+                    <button type="button" class="btn btn-primary" data-toggle="collapse">Calendar</button>
+                    <button type="button" class="btn btn-primary" data-toggle="collapse">Themes</button>
+                </div>
+            </div>
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+            <div class="right-side col-md-10">
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
             </div>
         </div>
-    </body>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<footer class="text-dark text-center">
+    <div class="container-fluid p-4">
+        <div><strong>&copy; 2018 Link|Me Website &#124; Design by: Joeren Aquino</strong></div>
+    </div>
+</footer>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://unpkg.com/ionicons@4.4.4/dist/ionicons.js"></script>
+<script src="js/script.js"></script>
+</body>
 </html>
